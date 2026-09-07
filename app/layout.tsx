f2970f5269bc019f44e7eb31bbf31fb884a1444b
Ailsa Gunn <ailsagunn@icloud.com>
2026-09-07 15:15:31 +0100

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link
+          rel="apple-touch-icon"
+          href="/icons/apple-touch-icon.png"
+        />
+      </head>
+
       <body className="min-h-full flex flex-col">
         <LanguageProvider>
           {children}
